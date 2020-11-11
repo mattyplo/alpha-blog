56ddbf015651652655f8b057bea6e0157c6228f7
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-11-10 20:46:14 -0800

    Styled messaging and alerts

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# get rid of field with errors setting when validation errors are hit
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
